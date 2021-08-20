031b847350b2b2e12e7bab49b986a2c7f6784aa5
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-08-20 17:20:31 +0200

    added layout structure

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
-<template> </template>
+<template>
+	<main>
+		<NavBar />
+		<div class="container flex">
+			<BillsTable />
+		</div>
+		<div class="w-1/2">
+			<Char :bills="activeBills" />
+		</div>
+	</main>
+</template>
 
 <script>
+import AddCategory from "./components/AddCategory.vue";
+import AddBill from "./components/AddBill.vue";
+import NavBar from "./components/NavBar.vue";
+import Chart from "./components/Chart.vue";
+import BillsTable from "./components/BillsTable.vue";
+
 export default {
 	name: "App",
 	components: {
-		HelloWorld,
+		AddCategory,
+		AddBill,
+		Chart,
+		BillsTable,
+		NavBar,
 	},
 };
 </script>
